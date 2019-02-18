98f327bf9982535005934b65ff45ee863d3c3235
katze <mike@lichat.net>
2019-02-18 23:23:09 +0100

    first commit

--- chatbot.php
+++ chatbot.php
@@ -2,5 +2,4 @@
 header('Content-type:application/json;charset=utf-8');
 $messages = json_decode(file_get_contents('story.json'), true);
 $response = $messages[$_GET['id']];
-$response['conversation_id'] = $_SESSION["conversation_id"];
 echo json_encode($response);
